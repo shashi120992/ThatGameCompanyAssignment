3ebc7b820867b82b37b6bfa0470ea7675156f50f
shashikanthbhat12 <shashi120992@gmail.com>
2022-03-02 07:14:28 +0530

    Updated commit made few changes in the grid

--- Assets/Scripts/Problem-1(TileGame)/GameManager.cs
+++ Assets/Scripts/Problem-1(TileGame)/GameManager.cs
@@ -13,6 +13,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Block mt_blockPrefab;
     [SerializeField] private Transform mt_CameraPosition;
     [SerializeField] private int mt_AreaOfIntrest;
+    //[SerializeField] Transform[] allcells;
+    //private GridLayout tilePos;
+
+    private GameObject node;
+    private GridLayout tileGrid;
 
     private void Start()
     {
@@ -23,6 +28,10 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
+            //Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Vector3Int tilePos = tileGrid.WorldToCell(worldPos);
+            //Debug.Log("cell is " + node);
             spawnBlocks();
         }
     }
@@ -32,9 +41,8 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < mt_height; j++)
             {
-                var node = Instantiate(mt_nodePrefab, new Vector3(i, j), Quaternion.identity);
-                node.name = $"Node_{i}{j}";
-
+                node = Instantiate(mt_nodePrefab, new Vector3(i, j), Quaternion.identity);
+                node.name = $"Node_{i}{j}";    
             }
         }
         mt_CameraPosition.transform.position = new Vector3((float)mt_width / 2 - 0.5f, (float)mt_width / 2 - 0.5f, -10);
@@ -60,10 +68,11 @@ public class GameManager : MonoBehaviour
             {
                 Vector3 a = new Vector3((float)Math.Round(worldPosition.x + i), (float)Math.Round(worldPosition.y + j), 0f);
                 var block1 = Instantiate(mt_blockPrefab, a, Quaternion.identity);
-                block1.init();
+                block1.transform.parent = node.transform;
+               // block1.init();
 
                 //Not Working...
-                /*
+
                 for (int k = 0; k < block1.transform.childCount; ++k)
                 {
                     Transform currentItem = block1.transform.GetChild(k);
@@ -71,18 +80,19 @@ public class GameManager : MonoBehaviour
                     //Search by name
                     if (currentItem.name.Equals("Back"))
                     {
-                        currentItem.GetComponent<Renderer>().material.color = GetRandomColour32();
+                        currentItem.GetComponent<SpriteRenderer>().material.color = GetRandomColour32();
+                        //currentItem.GetComponent<Sprite>().gra
                         continue;
                     }
 
                     if (currentItem.name.Equals("Text"))
                     {
-                        int s = Rand - 4 * mt_AreaOfIntrest;
+                        int s = Rand + ((-2 * mt_AreaOfIntrest) - 1)*j + i;
                         currentItem.GetComponentInChildren<TextMeshPro>().text = s.ToString();
                         continue;
                     }
                 }
-                */
+                
             }
         }
 
